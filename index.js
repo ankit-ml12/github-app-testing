7bd2caf6f039db77a2193e3e4f099d3d8dfe4d9d
Ankit Patidar <95223229+ankit-ml12@users.noreply.github.com>
2023-09-27 09:37:30 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function fibonacci(n) {
+  if (n <= 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+// Example usage:
+const n = 10; // Change this to the desired Fibonacci number you want to calculate
+const result = fibonacci(n);
+console.log(`The ${n}th Fibonacci number is: ${result}`);
